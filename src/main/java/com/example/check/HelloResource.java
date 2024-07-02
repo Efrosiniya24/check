c04d139cebdc1afe5457b58ed6bbeb1ae74f7c20
frosya <efrosiniya24@gmail.com>
2024-07-02 19:55:27 +0300

    .

--- src/main/java/com/example/check/HelloResource.java
+++ src/main/java/com/example/check/HelloResource.java
@@ -9,6 +9,6 @@ public class HelloResource {
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Helageslwo, Worjldd!";
+        return "Helageslwffo, Worjldd!";
     }
 }
