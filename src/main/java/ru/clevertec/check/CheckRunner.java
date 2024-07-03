932d85ec7a384931766380f0a1770d643903337b
frosya <efrosiniya24@gmail.com>
2024-07-03 18:22:01 +0300

    .

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -1,4 +1,6 @@
-public class Main {
+package main.java.ru.clevertec.check;
+
+public class CheckRunner {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
